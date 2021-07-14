5e10f3741e93fb612a2377fa79bcc19e0cecb274
rickykise <rickykise@naver.com>
2021-07-14 16:52:52 +0900

    210714

--- src/main/java/com/example/restfulwebservice/user/UserController.java
+++ src/main/java/com/example/restfulwebservice/user/UserController.java
@@ -1,9 +1,10 @@
 package com.example.restfulwebservice.user;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -22,6 +23,52 @@ public class UserController {
 
     @GetMapping("/users/{id}")
     public User retrieveUser(@PathVariable int id) {
-        return service.findOne(id);
+        User user = service.findOne(id);
+
+        if (user == null) {
+            throw new UserNotFoundException(String.format("ID[%s] not found", id));
+        }
+
+        return user;
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User savedUser = service.save(user);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
+    }
+
+    @DeleteMapping("/users/{id}")
+    public void deleteUser(@PathVariable int id) {
+        User user = service.deleteById(id);
+
+        if (user == null) {
+            throw new UserNotFoundException(String.format("ID[%s] not found", id));
+        }
+    }
+
+    @PutMapping("/users")
+    public ResponseEntity<User> updateUser(@RequestBody User user) {
+
+        User checkUser = service.findOne(user.getId());
+
+        if (checkUser == null) {
+            throw new UserNotFoundException(String.format("ID[%s] not found", user.getId()));
+        }
+
+        User updateUser = service.update(user);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(updateUser.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
     }
 }
--- src/main/java/com/example/restfulwebservice/user/UserDaoService.java
+++ src/main/java/com/example/restfulwebservice/user/UserDaoService.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
 
 @Service
@@ -22,7 +23,7 @@ public class UserDaoService {
         return users;
     }
 
-    public  User save(User user) {
+    public User save(User user) {
         if (user.getId() == null) {
             user.setId(++userCount);
         }
@@ -31,12 +32,40 @@ public class UserDaoService {
         return user;
     }
 
+    public User update(User getUser) {
+
+        for (User user: users) {
+            if (user.getId() == getUser.getId()) {
+                user.setName(getUser.getName());
+                user.setJoinDate(getUser.getJoinDate());
+            }
+        }
+
+        return getUser;
+    }
+
     public User findOne(int id) {
         for (User user: users) {
             if (user.getId() == id) {
                 return user;
             }
         }
+        return null;
+    }
+
+    public User deleteById(int id) {
+        Iterator<User> iterator = users.iterator();
+
+        while (iterator.hasNext()) {
+            User user = iterator.next();
+
+            if (user.getId() == id) {
+                iterator.remove();
+
+                return user;
+            }
+        }
+
         return null;
     }
 }
